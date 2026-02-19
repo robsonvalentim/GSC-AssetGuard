591b92f2a730e36986963a94d8a6d93aad24c0fa
Robson Valentim <robson.valentim@outlook.com>
2026-02-19 18:35:05 -0300

    feat: create asset entity and define core status machine including overdue state

---
+++ api-assetguard/src/assets/entities/asset.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum AssetStatus {
+  AVAILABLE = 'AVAILABLE',
+  IN_USE = 'IN_USE',
+  MAINTENANCE = 'MAINTENANCE',
+  OVERDUE = 'OVERDUE', 
+}
+
+@Entity('assets')
+export class Asset {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  description!: string;
+
+  @Column({ unique: true })
+  serialNumber!: string;
+
+  @Column({
+    type: 'varchar',
+    default: AssetStatus.AVAILABLE,
+  })
+  status!: AssetStatus;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
