8cf2dfce066d87e41ea6c89bed371c940acfe4ad
Robson Galdino Valentim <robson.valentim@gruposc.com.br>
2026-02-19 15:11:43 -0300

    feat: implement environment variables configuration

--- api-assetguard/src/app.module.ts
+++ api-assetguard/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    // Inicia o leitor de .env globalmente
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.sqlite',
--- api-assetguard/src/main.ts
+++ api-assetguard/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Lê a porta do .env ou usa 3000 como fallback
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Servidor AssetGuard rodando na porta: ${port}`);
 }
 bootstrap();
